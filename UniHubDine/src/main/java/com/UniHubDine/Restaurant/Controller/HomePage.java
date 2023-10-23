07f9b2b1685395132cca0f6e0f029b72ef288458
pavan <pavan@PavanKumarGajula>
2023-10-23 14:37:28 -0400

    New changes

--- UniHubDine/src/main/java/com/UniHubDine/Restaurant/Controller/HomePage.java
+++ UniHubDine/src/main/java/com/UniHubDine/Restaurant/Controller/HomePage.java
@@ -2,13 +2,18 @@ package com.UniHubDine.Restaurant.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomePage {
-	
+	@RequestMapping("home")
+	@ResponseBody()
+	public String home() {
+		return "hello world";
+	}
 	@RequestMapping("homepage")
 	public String homePage() {
-		return "HomePage";
+		return "NewFile";
 	}
 
 }
