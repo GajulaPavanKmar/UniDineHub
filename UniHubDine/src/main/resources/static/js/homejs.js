34636e44c1e1bf76bf81509df60210ef40fa0247
pavan <pavan@PavanKumarGajula>
2023-11-14 02:03:11 -0500

    Design changes

--- UniHubDine/src/main/resources/static/js/homejs.js
+++ UniHubDine/src/main/resources/static/js/homejs.js
@@ -1,17 +1,47 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuButtons = document.querySelectorAll('.menu-btn');
+function changeSlide(slideIndex) {
+    const slides = document.querySelector('.slides');
+    slides.style.transform = `translateX(-${slideIndex * 100}%)`;
+}
+function changeContent(direction) {
+    var contents = document.getElementsByClassName('carousel-content');
+    var currentIndex;
 
-    menuButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            const selectedMenu = this.getAttribute('data-menu');
+    // Find the currently displayed content
+    for (var i = 0; i < contents.length; i++) {
+        if (contents[i].style.display !== 'none') {
+            currentIndex = i;
+            break;
+        }
+    }
 
-            // Hide all menu sections
-            document.querySelectorAll('.menu-section').forEach(section => {
-                section.style.display = 'none';
-            });
+    var nextIndex = currentIndex + direction;
+    if (nextIndex >= contents.length) {
+        nextIndex = 0;
+    } else if (nextIndex < 0) {
+        nextIndex = contents.length - 1;
+    }
 
-            // Display the selected menu section
-            document.getElementById(selectedMenu).style.display = 'block';
-        });
-    });
-});
+    contents[currentIndex].style.left = direction > 0 ? '-100%' : '100%';
+    contents[nextIndex].style.display = 'block';
+    contents[nextIndex].style.left = '0';
+}
+
+var currentIndex = 1;
+showContent(currentIndex);
+
+function changeContent(n) {
+    showContent(currentIndex += n);
+}
+
+function showContent(n) {
+    var i;
+    var contents = document.getElementsByClassName("carousel-content");
+    if (n > contents.length) {currentIndex = 1}    
+    if (n < 1) {currentIndex = contents.length}
+    for (i = 0; i < contents.length; i++) {
+        contents[i].style.display = "none";  
+    }
+    contents[currentIndex - 1].style.display = "block";  
+}
+
+
